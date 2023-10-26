e6ed8dcff76862a9718c5da250066f2058de9219
trikytrukos <miguelmartinezsantos2003@gmail.com>
2023-10-26 13:30:59 +0200

    adaptando y añadiendo pequeñas modificaciones al nuevo index

---
+++ public/pag-principal max/main.js
@@ -0,0 +1,20 @@
+function desplegar() {
+    let elemento = document.getElementById("boton");
+    let dropdown = document.getElementById("dropdown");
+
+    elemento.addEventListener("mouseenter", (evento) => {
+        if (dropdown.style.display === "block" || dropdown.classList.contains("show")) {
+            dropdown.style.display = "none";
+            dropdown.classList.remove("show");
+        } else {
+            dropdown.style.display = "block";
+            dropdown.classList.add("show");
+        }
+    });
+
+    dropdown.addEventListener("mouseleave", (evento) => {
+        dropdown.style.display = "none";
+        dropdown.classList.remove("show");
+    });
+}
+desplegar();
