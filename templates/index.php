8b426490f57db9e0b52717b2d2b4cab679801781
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-19 03:27:54 +0100

    Eliminado CDN de fontawesome. Añadido friconix.

--- templates/index.php
+++ templates/index.php
@@ -2,8 +2,9 @@
 $descripcion = 'Página de Login/Registro de AsynCore';
 $titulo = 'LOGIN / REGISTRO';
 $css = ['/css/bootstrap/bootstrap.min.css', '/css/mdb/mdb.min.css', '/css/login-registro-style.css'];
-$js = ['js/main-main.js', '/js/bootstrap/bootstrap.bundle.js', '/js/mdb/mdb.min.js'];
-$cdn = "<script defer crossorigin='anonymous' src='https://kit.fontawesome.com/9e6ce9bbf3.js' type='text/javascript'></script>";
+$js = ['js/main-main.js', '/js/bootstrap/bootstrap.bundle.js', '/js/mdb/mdb.min.js', 'https://friconix.com/cdn/friconix.js'];
+$url = $_SERVER['HTTP_HOST'];
+include_once '../src/head.php';
 include_once '../src/header.php';
 ?>
 <main>
