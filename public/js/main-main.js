237bd2cfd851b3335e8b5e16ee9374cea7332533
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-19 03:27:03 +0100

    Añadido evento DOMContentLoaded.

--- public/js/main-main.js
+++ public/js/main-main.js
@@ -1,21 +1,23 @@
-function desplegar() {
-    let elemento = document.getElementById("boton");
-    let dropdown = document.getElementById("dropdown");
+document.addEventListener("DOMContentLoaded", () => {
+    function desplegar() {
+        let elemento = document.getElementById("boton");
+        let dropdown = document.getElementById("dropdown");
 
-    elemento.addEventListener("mouseenter", (evento) => {
-        if (dropdown.style.display === "block" || dropdown.classList.contains("show")) {
+        elemento.addEventListener("mouseenter", (evento) => {
+            if (dropdown.style.display === "block" || dropdown.classList.contains("show")) {
+                dropdown.style.display = "none";
+                dropdown.classList.remove("show");
+            } else {
+                dropdown.style.display = "block";
+                dropdown.classList.add("show");
+            }
+        });
+
+        dropdown.addEventListener("mouseleave", (evento) => {
             dropdown.style.display = "none";
             dropdown.classList.remove("show");
-        } else {
-            dropdown.style.display = "block";
-            dropdown.classList.add("show");
-        }
-    });
+        });
+    }
 
-    dropdown.addEventListener("mouseleave", (evento) => {
-        dropdown.style.display = "none";
-        dropdown.classList.remove("show");
-    });
-}
-
-desplegar();
+    desplegar();
+});
