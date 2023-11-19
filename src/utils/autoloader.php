33a3c8be35ef6fc901bd11cc4b73810cebd425b3
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-19 03:25:31 +0100

    Añadido BR para debugging.

--- src/utils/autoloader.php
+++ src/utils/autoloader.php
@@ -11,7 +11,7 @@ spl_autoload_register(function ($class) {
     //echo '__DIR__: ' . __DIR__ . PHP_EOL . "<br>";
     //echo 'DOCUMENT_ROOT: ' . $_SERVER['DOCUMENT_ROOT'] . PHP_EOL . '<br>';
     $file = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . $prefix . '.php';
-    //echo 'File path: ' . $file . PHP_EOL;
+    //echo 'File path: ' . $file . '<br>';
     if (file_exists($file)) {
         require $file;
     }
