bfe19e642a6db0ea61f9bd7767e51560ee1a5c14
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-21 00:21:55 +0100

    Nuevos scripts de CSS y JS para el nuevo template en su ubicación final.

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var btnHamburger = document.querySelector('.btn-hamburger');
-    var navbar = document.querySelector('.navbar');
-    var userDropdown = document.getElementById('user-dropdown');
+    let btnHamburger = document.querySelector('.btn-hamburger');
+    let navbar = document.querySelector('.navbar');
+    let userDropdown = document.getElementById('user-dropdown');
 
     btnHamburger.addEventListener('click', function () {
         navbar.classList.toggle('navbar-open');
     });
 
     // Agrega un controlador de eventos para el desplegable del usuario
-    var userTrigger = document.querySelector('.navbar-user a');
+    let userTrigger = document.querySelector('.navbar-user a');
     userTrigger.addEventListener('click', function (e) {
         e.preventDefault(); // Previene la acción predeterminada del enlace
         userDropdown.classList.toggle('active-drop');
