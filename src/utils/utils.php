e74214cecf424fd6671a1f688be487433a331fe3
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-20 09:52:41 +0100

    Añadida documentación y pequeños cambios para mejorar la legibilidad del código. Uso de ENT_QUOTES en htmlspecialchars()

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -2,20 +2,65 @@
     /**
      * utils.php
      *
-     * Archivo con funciones que proporcionan utilidad.
-     * Contiene funciones para formatear fechas, redirigir a páginas, validar formularios, etc.
+     * Script con funciones que proporcionan utilidad.
+     * Contiene funciones para formatear fechas, redirigir a páginas, validar formularios, obtener mensajes de error,
+     * imprimir información, etc.
      *
      * @package utils
      * @version 1.0.0
      * @auhor   Daniel Alonso Lázaro <dalonsolaz@gmail.com>
      */
     
-    /* FECHAS */
+    /* CONSTANTES CON EL HTML PARA LOS MODALES DE INFO, ÉXITO Y ALERTA */
+    const INFO_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                        </symbol>
+                    </svg>';
+    
+    const SUCCESS_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                            </symbol>
+                        </svg>';
+    
+    const ALERT_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </symbol>
+                        </svg>';
+    
+    const INFO = '<div class="alert alert-primary d-flex align-items-center" role="alert">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
+                    <div class="d-flex align-items-center flex-column">
+                        {message}
+                    </div>';
+    
+    const SUCCESS = '<div class="alert alert-success d-flex align-items-center" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                        <div class="d-flex align-items-center flex-column">
+                            {message}
+                        </div>';
+    
+    const WARNING = '<div class="alert alert-warning d-flex align-items-center" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                        <div class="d-flex align-items-center flex-column">
+                            {message}
+                        </div>;
+                    </div>';
+    
+    const DANGER = '<div class="alert alert-danger d-flex align-items-center" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                        <div class="d-flex align-items-center flex-column">
+                            {message}
+                        </div>;
+                    </div>';
+    
     /**
      * Función que cambia el formato de una fecha.
      *
      * Lo más usual es que la fecha venga en formato Y-m-d H:i:s y se quiera mostrar en formato d/m/Y H:i:s.
-     * También aceptaría d/m/y o d-m-y o cualquier combinación de ellos.
+     * También aceptaría d/m/y o d-m-y o cualquier combinación de ellas.
      *
      * @param string $date   Fecha en formato Y-m-d H:i:s.
      * @param string $format Formato de la fecha.
@@ -27,7 +72,6 @@
         return date_format(date_create($date), $format);
     }
     
-    /* FORMULARIOS */
     /**
      * Función para sanear los datos del formulario.
      *
@@ -48,7 +92,7 @@
         }
         $data = trim(strtolower($data));
         $data = stripslashes($data);
-        return htmlspecialchars($data);
+        return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
     }
     
     /**
@@ -120,92 +164,85 @@
         return EMPTY_STRING;
     }
     
-    /* MENSAJES */
-    const INFO_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                        </symbol>
-                    </svg>';
-    
-    const SUCCESS_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                            </symbol>
-                        </svg>';
-    
-    const ALERT_SVG = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                            </symbol>
-                        </svg>';
-    
-    const INFO = '<div class="alert alert-primary d-flex align-items-center" role="alert">
-                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
-                    <div class="d-flex align-items-center flex-column">
-                        {message}
-                    </div>';
-    
-    const SUCCESS = '<div class="alert alert-success d-flex align-items-center" role="alert">
-                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-                        <div class="d-flex align-items-center flex-column">
-                            {message}
-                        </div>';
-    
-    const WARNING = '<div class="alert alert-warning d-flex align-items-center" role="alert">
-                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                        <div class="d-flex align-items-center flex-column">
-                            {message}
-                        </div>;
-                    </div>';
-    
-    const DANGER = '<div class="alert alert-danger d-flex align-items-center" role="alert">
-                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                        <div class="d-flex align-items-center flex-column">
-                            {message}
-                        </div>;
-                    </div>';
-    
     /**
-     * @param string $username Nombre de usuario
+     * Imprime un mensaje de éxito de inicio de sesión y proporciona una redirección.
      *
-     * @return string Devuelve un string con el mensaje de login correcto y el aviso de redirección.
+     * @param string $username El nombre de usuario para el cual se está imprimiendo el mensaje de éxito.
+     *
+     * @returns string Devuelve un string HTML que incluye un mensaje de bienvenida personalizado para el usuario y una redirección a la página principal.
      */
     function printLoginSuccess(string $username): string
     {
         $url = 'https://' . $_SERVER['HTTP_HOST'] . '/' . 'main.php';
-        $message = '<div class="mb-3" style="margin-left: 20px;">¡Bienvenido <b>' . $username . '</b>!</div>' . PHP_EOL .
-            '<div style="margin-left: 20px;">Redirigiendo a la página principal...<br><span class="small">Si no se redirige en 5 segundos, pulsa <a href="' . $url . '">aquí</a></span></div>';
+        $message = <<<HTML
+                    <div class="mb-3" style="margin-left: 20px;">¡Bienvenido <b>$username</b>!</div>
+                    <div style="margin-left: 20px;">Redirigiendo a la página principal...<br>
+                        <span class="small">Si no se redirige en 10 segundos, pulsa <a href="$url">aquí</a></span>
+                    </div>
+                    HTML;
         return SUCCESS_SVG . PHP_EOL . str_replace('{message}', $message, SUCCESS);
     }
     
     /**
-     * @return string Devuelve un string con el mensaje de login incorrecto.
+     * Imprime un mensaje de error de inicio de sesión.
+     *
+     * @returns string Devuelve un string HTML que indica un error en el inicio de sesión, como un nombre de usuario o contraseña incorrectos.
      */
     function printLoginFail(): string
     {
         $message = '<div style="margin-left: 20px;">Nombre de usuario o contraseña incorrectos.</div>';
         return ALERT_SVG . PHP_EOL . str_replace('{message}', $message, DANGER);
     }
+    
     /**
-     * @return string Devuelve un string con el mensaje de login incorrecto.
+     * Imprime un mensaje de error de registro.
+     *
+     * Esta función también utiliza 'returnSQLError()' para detallar el mensaje de error basándose en el error de SQL.
+     *
+     * @param string $username El nombre de usuario para el cual se intentó el registro.
+     *
+     * @return string Devuelve un string HTML que incluye un mensaje de error específico para el intento fallido de registro del usuario.
+     * @see returnSQLError()
      */
-    function printRegisterFail($username): string
-    {
-        $message = '<div style="margin-left: 20px;">No se ha podido registrar al usuario <b>' . $username . '</b>.<br>' . returnSQLError() . '</div>';
+    function printRegisterFail(string $username): string {
+        $sqlError = returnSQLError();
+        $message = <<<HTML
+                    <div style="margin-left: 20px;">
+                        No se ha podido registrar al usuario <b>$username</b>.<br>
+                        $sqlError
+                    </div>
+                    HTML;
+        
         return ALERT_SVG . PHP_EOL . str_replace('{message}', $message, DANGER);
     }
     
     /**
-     * @param string $username Nombre de usuario
+     * Imprime un mensaje de éxito de registro.
      *
-     * @return string Devuelve un string con el mensaje de registro correcto.
+     * @param string $username El nombre de usuario que se ha registrado con éxito.
+     *
+     * @return string Devuelve un string HTML que incluye un mensaje de confirmación de registro exitoso para el usuario.
      */
-    function printRegisterSuccess(string $username): string
-    {
-        $message = '<div style="margin-left: 20px;">Usuario <b>' . $username . '</b> registrado correctamente.<br>Inicia sesión para continuar...</div>';
+    function printRegisterSuccess(string $username): string {
+        $message = <<<HTML
+                    <div style="margin-left: 20px;">
+                        Usuario <b>$username</b> registrado correctamente.<br>
+                        Inicia sesión para continuar...
+                    </div>
+                    HTML;
+        
         return SUCCESS_SVG . PHP_EOL . str_replace('{message}', $message, SUCCESS);
     }
     
+    /**
+     * Devuelve un mensaje de error específico basado en el error de SQL ocurrido.
+     *
+     * Dependencias:
+     *   - Utiliza $_SESSION['USERMANAGER_SQL_ERROR'] para determinar el tipo de error SQL.
+     *   - Depende de los valores de sesión $_SESSION['registerEmail'] y $_SESSION['registerUserName'].
+     *
+     * @return string Mensaje de error basado en el error de SQL.
+     */
     function returnSQLError(): string
     {
         if (str_contains($_SESSION['USERMANAGER_SQL_ERROR'], 'EMAIL')) {
@@ -218,6 +255,13 @@
     }
     
     /**
+     * Limpia las variables de sesión relacionadas con el inicio de sesión y el registro.
+     *
+     * Previene el posible uso indebido de datos residuales.
+     *
+     * Efectos Secundarios:
+     *   - Elimina todas las variables de sesión utilizadas en el proceso de autenticación.
+     *
      * @return void
      */
     function unsetLoginRegister(): void
