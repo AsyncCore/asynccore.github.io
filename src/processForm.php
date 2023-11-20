9a35722d64edfa704c6d87af3f50efb5daff8d5f
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-20 10:09:46 +0100

    Cambiada lógica de urls GET en redirecciones del usuario durante el login y el registro. Utilizadas constantes para imprimir NAME_LENGTH_ERROR Y USERNAME_LENGTH_ERROR. Mejora de PHPDOCS. Añadido try-catch para error en la generación del token con redirección a página de error de DB.

--- src/processForm.php
+++ src/processForm.php
@@ -1,22 +1,52 @@
 <?php
-    /* Iniciar sesión para manejar la información del usuario */
     
     use src\managers\UserManager;
     use src\db\DatabaseConnection;
     use src\managers\TokenManager;
     
-    /* Importar funciones de utilidades */
+    /* Importar funcionalidad global */
     require 'utils/sessionInit.php';
     require_once 'utils/utils.php';
     
     /* Definición de constantes */
     /* INT */
+    /**
+     * @const NAMES_MIN_LENGTH Longitud mínima de los nombres.
+     * @const NAMES_MAX_LENGTH Longitud máxima de los nombres.
+     * @const PASSWORD_MIN_LENGTH Longitud mínima de las contraseñas.
+     * @const PASSWORD_MAX_LENGTH Longitud máxima de las contraseñas.
+     * @const COOKIE_TIME Tiempo de vida de las cookies.
+     */
     const NAMES_MIN_LENGTH = 3;
     const NAMES_MAX_LENGTH = 50;
     const PASSWORD_MIN_LENGTH = 8;
     const PASSWORD_MAX_LENGTH = 16;
+    define('COOKIE_TIME', time() + 60 * 60 * 24 * 7);
     
     /* STRING */
+    /**
+     * @const ACTIVE_TAB_DEFAULT Tab activa por defecto en el formulario.
+     * @const ACTIVE_TAB_REGISTER Tab activa para ir al formulario de registro.
+     * @const CHECKED Valor del checkbox cuando está marcado.
+     * @const EMPTY_STRING Cadena vacía.
+     * @const MAIL_ERROR Mensaje de error para el campo de email.
+     * @const MAIL_PATTERN Patrón para validar el campo de email.
+     * @const MAIL_REQUIRED_ERROR Mensaje de error para el campo de email cuando está vacío.
+     * @const NAME_COMPARISON_DEFAULT Nombre de la variable para diferenciar el nombre del usuario del nombre real.
+     * @const NAME_ERROR Mensaje de error para el campo de nombre cuando no cumple el patrón.
+     * @const NAME_LENGTH_ERROR Mensaje de error para el campo de nombre cuando no cumple la longitud.
+     * @const NAME_PATTERN Patrón para validar el campo de nombre.
+     * @const NAME_REQUIRED_ERROR Mensaje de error para el campo de nombre cuando está vacío.
+     * @const PASSWORD_LENGTH_ERROR Mensaje de error para el campo de contraseña cuando no cumple la longitud.
+     * @const PASSWORD_NOT_EQUAL_ERROR Mensaje de error para el campo de repetir contraseña cuando no coincide con el campo de contraseña.
+     * @const PASSWORD_REQUIRED_ERROR Mensaje de error para el campo de contraseña cuando está vacío.
+     * @const TERMS_ERROR Mensaje de error para el campo de términos y condiciones cuando no está marcado.
+     * @const UNCHECKED Valor del checkbox cuando no está marcado.
+     * @const USERNAME_PATTERN Patrón para validar el campo de nombre de usuario.
+     * @const USERNAME_REQUIRED_ERROR Mensaje de error para el campo de nombre de usuario cuando está vacío.
+     * @const USERNAME_LENGTH_ERROR Mensaje de error para el campo de nombre de usuario cuando no cumple la longitud.
+     * @const USERNAME_ERROR Mensaje de error para el campo de nombre de usuario cuando no cumple el patrón.
+     */
     const ACTIVE_TAB_DEFAULT = 'login';
     const ACTIVE_TAB_REGISTER = 'registro';
     const CHECKED = '1';
@@ -24,10 +54,9 @@
     const MAIL_ERROR = "El Email debe ser de EducaMadrid <br> Ejemplo: 'usuario@educa.madrid.org'";
     const MAIL_PATTERN = '/^[a-zA-Z0-9_.+-]+@educa\.madrid\.org$/';
     const MAIL_REQUIRED_ERROR = 'El Email es obligatorio';
-    const METHOD_POST = 'POST';
     const NAME_COMPARISON_DEFAULT = 'registerName';
     const NAME_ERROR = 'Solo se permiten letras y espacios';
-    const NAME_LENGTH_ERROR = 'El nombre debe tener entre 3 y 50 caracteres';
+    const NAME_LENGTH_ERROR = 'El nombre debe tener entre ' . NAMES_MIN_LENGTH . ' y ' . NAMES_MAX_LENGTH . ' caracteres';
     const NAME_PATTERN = '/^[a-zA-ZÀ-ÖØ-öø-ÿ ]+$/';
     const NAME_REQUIRED_ERROR = 'El nombre es obligatorio';
     const PASSWORD_LENGTH_ERROR = 'La contraseña debe tener entre ' . PASSWORD_MIN_LENGTH . ' y ' . PASSWORD_MAX_LENGTH . ' caracteres';
@@ -37,17 +66,17 @@
     const UNCHECKED = '0';
     const USERNAME_PATTERN = '/^[a-zA-Z0-9_.-]+$/';
     const USERNAME_REQUIRED_ERROR = 'El nombre de usuario es obligatorio';
-    const USERNAME_LENGTH_ERROR = 'El nombre de usuario debe tener entre 3 y 50 caracteres';
+    const USERNAME_LENGTH_ERROR = 'El nombre de usuario debe tener entre ' . NAMES_MIN_LENGTH . ' y ' . NAMES_MAX_LENGTH . ' caracteres';
     const USERNAME_ERROR = 'Solo se permiten letras, números y los caracteres especiales: _ . -';
-    define("COOKIE_TIME", time() + 60 * 60 * 24 * 7);
     
     /* Conexión a la base de datos */
     $db = DatabaseConnection::getInstance()->getConnection();
     
     /* Instancia de UserManager */
     $userManager = new UserManager($db);
-
-// Variables para array de errores, valores del formulario, tab activa, cookies y sesiones
+    
+    /* VARIABLES */
+    /* Variables generales del script */
     $activeTab = $_POST['form'] ?? ACTIVE_TAB_DEFAULT;
     $errores = [];
     $cookieName = EMPTY_STRING;
@@ -74,8 +103,8 @@
     $_SESSION['registerPasswordError'] = $_SESSION['registerPasswordError'] ?? EMPTY_STRING;
     $_SESSION['registerRepeatPasswordError'] = $_SESSION['registerRepeatPasswordError'] ?? EMPTY_STRING;
     $_SESSION['registerCheckError'] = $_SESSION['registerCheckError'] ?? EMPTY_STRING;
-
-// Verificar si se envió el formulario
+    
+    /* Comprobaciones de validación */
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['form'] === 'login') {
         $_SESSION['loginEmail'] = sanitizeData($_POST['loginEmail']);
         $_SESSION['loginPassword'] = sanitizeData($_POST['loginPassword']);
@@ -88,14 +117,18 @@
         if (!array_filter($errores)) {
             $usuario = $userManager->login($_SESSION['loginEmail'], $_SESSION['loginPassword']);
             if (!$usuario) {
-                header('Location: login-register.php?login&error');
+                header('Location: login-register.php?loginTab&error&login');
             } else {
                 if ($_SESSION['loginCheck'] === CHECKED) {
                     $tokenManager = new TokenManager($db);
-                    $tokenManager->generateToken($usuario['USER_ID']);
-                    $cookieName = 'usuario' . $usuario['USER_ID'];
-                    $cookieValue = $usuario['USERNAME'];
-                    setcookie($cookieName, $cookieValue, COOKIE_TIME, '/', true, true);
+                    try{
+                        $token = $tokenManager->generateToken($usuario['USER_ID']);
+                        $cookieName = 'usuario' . $usuario['USER_ID'];
+                        $cookieValue = $token;
+                        setcookie($cookieName, $cookieValue, COOKIE_TIME, '/', true, true);
+                    }catch (Exception $e) {
+                        header("Location: error-pages/dbError.php");
+                    }
                 }
                 $_SESSION['NAME'] = $usuario['NAME'];
                 $_SESSION['USERNAME'] = $usuario['USERNAME'];
@@ -105,7 +138,7 @@
                 $_SESSION['FECHA_REGISTRO'] = $usuario['FECHA_REGISTRO'];
                 $_SESSION['TIPO_USUARIO'] = $usuario['TIPO_USUARIO'];
                 unsetLoginRegister();
-                header('Location: login-register.php?login&success');
+                header('Location: login-register.php?loginTab&success&login');
             }
             die;
         }
@@ -135,13 +168,13 @@
         if (!array_filter($errores)) {
             $usuario = $userManager->register($_SESSION['registerName'], $_SESSION['registerUserName'], $_SESSION['registerEmail'], $_SESSION['registerPassword']);
             if (!$usuario) {
-                header('Location: login-register.php?register&error');
+                header('Location: login-register.php?registerTab&error&register');
             } else {
                 $_SESSION['loginEmail'] = $_SESSION['registerEmail'];
                 $_SESSION['loginPassword'] = $_SESSION['registerPassword'];
                 header('Location: login-register.php?register&success&loginTab');
             }
-        }else{
-            header('Location: login-register.php?register');
+        } else {
+            header('Location: login-register.php?registerTab');
         }
     }
