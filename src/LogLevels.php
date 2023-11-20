f83178962c699f7aaf9a23cb43c943cb6eb8ffa3
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-20 10:11:09 +0100

    Añadido default y mejorada documentación.

--- src/LogLevels.php
+++ src/LogLevels.php
@@ -1,15 +1,16 @@
 <?php
-
     namespace src;
-
+    
     /**
-    * Enum con los tipos de mensajes de log.
-    * Puede ser: "INFO", "WARNING", "ERROR", "EXCEPTION" o "DEBUG".
-    * @author  Daniel Alonso Lázaro <dalonsolaz@gmail.com>
-    * @access  public
-    * @package src
-    */
-
+     * Enum con los tipos de mensajes de log.
+     * Puede ser: "INFO", "WARNING", "ERROR", "EXCEPTION" o "DEBUG".
+     * El caso default si no se proporciona uno es "LOG".
+     *
+     * @see     Logger
+     * @author  Daniel Alonso Lázaro <dalonsolaz@gmail.com>
+     * @access  public
+     * @package src
+     */
     enum LogLevels: string
     {
         case INFO = "INFO";
@@ -17,4 +18,5 @@
         case ERROR = "ERROR";
         case EXCEPTION = "EXCEPTION";
         case DEBUG = "DEBUG";
+        case DEFAULT = "LOG";
     }
