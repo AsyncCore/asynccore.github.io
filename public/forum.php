b4c38c56ce0e4a159ba7c26ae1b6a965705ec1d8
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-23 12:29:17 +0100

    Cambios en la lógica para manejar errores de GET.

--- public/forum.php
+++ public/forum.php
@@ -18,7 +18,7 @@
     
     $descripcion = "Foro de AsynCore. Un foro de programación para estudiantes de DAW y DAM.";
     $titulo = "Foro de AsynCore";
-    $css = ["css/style.css", "css/footer.css"];
+    $css = ["css/style.css", "css/mdb-custom.css", "css/forum.css", "css/footer.css"];
     $js = ["js/script.js"];
     $cdn = ["https://friconix.com/cdn/friconix.js"];
     include_once DIR . '/src/head.php';
@@ -33,11 +33,27 @@
     $threadManager = new ThreadManager($db);
     $userManager = new UserManager($db);
     $categories = $categoryManager->getAllCategories();
+    
+    $message = '';
+    if (isset($_GET['c'])) {
+        if ($_GET['c'] == 'nf') {
+            $message = <<<HTML
+                                <div>
+                                    <h2>404 - NOT FOUND</h2>
+                                    <p>La categoría que estás buscando no existe. Puedes <a href="/main.php">volver a la página principal</a>.</p>
+                                </div>
+HTML;
+            $message = printCategoryFail($message);
+        }
+    }
 ?>
     <main>
         <div class="container">
             <div class="col-full push-top">
                 <h1>Foro de Asyncore</h1>
+            </div>
+            <div class="col-full">
+                <?= $message ?>
             </div>
             <div class="col-full">
                 <div class="forum-list">
@@ -46,39 +62,42 @@
                     </h2>
                     <?php foreach ($categories as $category): ?>
                         <?php
-                            $lastThread = $threadManager->getLastThreadByCategoryWithUser($category['CAT_ID']);
-                            if($lastThread){
-                                $user = $userManager->getUserById($lastThread['USER_ID']);
-                            }
+                        $lastThread = $threadManager->getLastThreadByCategoryWithUser($category['CAT_ID']);
+                        if ($lastThread) {
+                            $user = $userManager->getUserById($lastThread['USER_ID']);
+                        }
                         ?>
                         <div class="forum-listing">
                             <div class="forum-icon">
-                                <img src="https://<?= $_SERVER['HTTP_HOST'] . $category['ICONO'] ?>" alt="Icono de la categoría <?=$category['TITULO']?>">
+                                <img src="https://<?= $_SERVER['HTTP_HOST'] . $category['ICONO'] ?>"
+                                     alt="Icono de la categoría <?= $category['TITULO'] ?>">
                             </div>
                             <div class="forum-details">
                                 <a class="text-xlarge"
                                    href="category.php?c=<?= $category['CAT_ID'] ?>"><?= $category['TITULO'] ?></a>
                                 <p><?= $category['SUBTITULO'] ?></p>
                             </div>
-    
+
                             <div class="threads-count">
                                 <p class="count"><?= $threadManager->getThreadCountByCategory($category['CAT_ID']) ?></p>
                                 hilos
                             </div>
                             <?php if ($lastThread): ?>
-                            <div class="last-thread">
-                                <img class='avatar'
-                                     src="<?= $user['AVATAR']?>"
-                                     alt="AVATAR DEL USUARIO <?= $user['USERNAME']?>">
-                                <div class="last-thread-details">
-                                    <a href="thread.php?c=<?= $category['CAT_ID'] ?>&t=<?=$lastThread['THREAD_ID']?>"><?= $lastThread['TITULO'] ?? 'Título del hilo' ?></a>
-                                    <p class="text-xsmall">By <a href="profile.php&UID=<?= $lastThread['USER_ID'] ?>"><?= $lastThread['USERNAME']?></a>, <?= timeAgo($lastThread['F_CRE'])?></p>
+                                <div class="last-thread">
+                                    <img class='avatar'
+                                         src="<?= $user['AVATAR'] ?>"
+                                         alt="AVATAR DEL USUARIO <?= $user['USERNAME'] ?>">
+                                    <div class="last-thread-details">
+                                        <a href="thread.php?c=<?= $category['CAT_ID'] ?>&t=<?= $lastThread['THREAD_ID'] ?>"><?= $lastThread['TITULO'] ?? 'Título del hilo' ?></a>
+                                        <p class="text-xsmall">By <a
+                                                    href="profile.php&UID=<?= $lastThread['USER_ID'] ?>"><?= $lastThread['USERNAME'] ?></a>, <?= timeAgo($lastThread['F_CRE']) ?>
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
                             <?php else: ?>
-                            <div class="last-thread">
-                                <p>No hay hilos en esta categoría</p>
-                            </div>
+                                <div class="last-thread">
+                                    <p>No hay hilos en esta categoría</p>
+                                </div>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
