a01479976fc8dc8d05d2b85ab5e9b5de2b20c771
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-24 11:35:58 +0100

    Rework del footer para eliminar heredoc. Añadida lógica para hacer funcionales los contadores.

--- src/footer.php
+++ src/footer.php
@@ -1,43 +1,43 @@
 <?php
-    
     use src\utils\Online;
     use src\managers\UserManager;
     use src\db\DatabaseConnection;
+    use src\managers\PostsManager;
+    use src\managers\ThreadManager;
     
     $db = DatabaseConnection::getInstance()->getConnection();
+    $threadManager = new ThreadManager($db);
+    $postManager = new PostsManager($db);
     $userManager = new UserManager($db);
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
     $usersOnline = Online::who();
     $usersOnline = $usersOnline == 1 ? $usersOnline . ' usuario online' : $usersOnline . ' usuarios online';
     $countUsersRegistered = $userManager->userCount();
     $countUsersRegistered = $countUsersRegistered == 1 ? $countUsersRegistered . ' usuario registrado' : $countUsersRegistered . ' usuarios registrados';
-    $countThreads = '0';
-    $countPosts = '0';
+    $countThreads = $threadManager->getThreadCount();
+    $countPosts = $postManager->getPostCount();
     
-    echo <<<HTML
-   <footer class="footer">
-        <div class="footer-container">
-        <div class="forum-stats desktop-only">
-            <ul>
-                <li><i class="fi-cnsuxl-user-circle"></i> {$usersOnline}</li>
-                <li><i class="fi-xnsuxl-user-solid"></i> {$countUsersRegistered}</li>
-                <li><i class="fi-xnsuxl-comment-dots-solid"></i> {$countThreads} threads</li>
-                <li><i class="fi-xnluxl-comment"></i> {$countPosts} posts</li>
-            </ul>
-        </div>
-        </div>
-        <div class="footer-container">
-            <div class="footer-content">
-                <div class="footer-left">
-                    <span>Copyright &copy; AsynCore 2023</span>
-                </div>
-                <div class="footer-right">
-                    <a href="https://friconix.com/" target="_blank" rel="noopener noreferrer" class="friconix">Friconix</a>
-                </div>
+?>
+<footer class="footer">
+    <div class="footer-container">
+    <div class="forum-stats desktop-only">
+        <ul>
+            <li><i class="fi-xnsuxl-power-solid" style="margin-right: 5px"></i><?= $usersOnline ?></li>
+            <li><i class="fi-xnsuxl-user-solid" style="margin-right: 5px"></i><?= $countUsersRegistered ?></li>
+            <li><i class="fi-xnsuxl-comment-dots-solid" style="margin-right: 5px"></i><?= $countThreads ?> hilos</li>
+            <li><i class="fi-xnluxl-comment" style="margin-right: 5px"></i><?= $countPosts ?> posts</li>
+        </ul>
+    </div>
+    </div>
+    <div class="footer-container">
+        <div class="footer-content">
+            <div class="footer-left">
+                <span>Copyright &copy; AsynCore 2023</span>
+            </div>
+            <div class="footer-right">
+                <a href="https://friconix.com/" target="_blank" rel="noopener noreferrer" class="friconix">Friconix</a>
             </div>
         </div>
-    </footer>
-    </body>
-    </html>
-HTML;
+    </div>
+</footer>
+</body>
+</html>
